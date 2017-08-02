2f96666546077577a3194870828bed2d515e0028
java2019 <java_2019@mail.ru>
2017-08-02 16:18:14 +0300

    init

--- app/src/main/java/con/nemo/fragmentconnectionlistview/MainActivity.java
+++ app/src/main/java/con/nemo/fragmentconnectionlistview/MainActivity.java
@@ -1,13 +1,33 @@
 package con.nemo.fragmentconnectionlistview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.lView);
+        listView.setAdapter(new ArrayAdapter<ArrayAdapter>();
+        listView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ActivityDetails.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
 }
