278b0b851bb1eadbae8a6e37024234e25129f9e2
java2019 <java_2019@mail.ru>
2017-08-02 15:56:43 +0300

    Initial commit

---
+++ app/src/main/java/con/nemo/fragmentconnectionlistview/ActivityDetails.java
@@ -0,0 +1,13 @@
+package con.nemo.fragmentconnectionlistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_details);
+    }
+}
